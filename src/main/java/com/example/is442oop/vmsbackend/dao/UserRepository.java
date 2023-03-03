bfde2800a9eacb66cd9ead53a63629b69cce281c
Liye Ng <liye1998@hotmail.com>
2023-03-03 10:12:25 +0800

    Added retrieveUsers endpoint (w/o JWT authentication)

--- src/main/java/com/example/is442oop/vmsbackend/dao/UserRepository.java
+++ src/main/java/com/example/is442oop/vmsbackend/dao/UserRepository.java
@@ -2,9 +2,14 @@ package com.example.is442oop.vmsbackend.dao;
 
 import com.example.is442oop.vmsbackend.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-  Optional<User> findStudentByEmail(String email);
+  Optional<User> findUserByEmail(String email);
+
+  @Query("SELECT u from User u")
+  List<User> getAllUsers();
 }
