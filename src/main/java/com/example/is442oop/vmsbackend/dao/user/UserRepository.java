6fbd09a7927a66941d909bb87062b6eb074b313e
randall yeo <randall.yeo@isobar.com>
2023-03-05 17:13:14 +0800

    resolve dependency bug

--- src/main/java/com/example/is442oop/vmsbackend/dao/user/UserRepository.java
+++ src/main/java/com/example/is442oop/vmsbackend/dao/user/UserRepository.java
@@ -6,14 +6,16 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
+@Component
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findUserByEmail(String email);
 
   @Modifying
   @Transactional
   @Query("UPDATE User u SET u.password = :password WHERE u.id = :id")
-  User updatePassword(@Param("id") Long id, @Param("name") String password);
+  User updatePassword(@Param("id") Long id, @Param("password") String password);
 }
--- src/main/java/com/example/is442oop/vmsbackend/entities/User.java
+++ src/main/java/com/example/is442oop/vmsbackend/entities/User.java
@@ -88,12 +88,6 @@ public class User {
     }
 
 
-    
-
-    public UserType getUserType() {
-        return userType;
-    }
-
     public void setUserType(UserType userType) {
         this.userType = userType;
     }
