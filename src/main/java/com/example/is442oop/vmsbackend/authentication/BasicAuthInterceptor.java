aebec02e158eb2fc6b49d2d015c57a976dd930b7
randall yeo <randall.yeo@isobar.com>
2023-03-06 17:55:46 +0800

    fix auth errors

--- src/main/java/com/example/is442oop/vmsbackend/authentication/BasicAuthInterceptor.java
+++ src/main/java/com/example/is442oop/vmsbackend/authentication/BasicAuthInterceptor.java
@@ -7,41 +7,44 @@ import com.example.is442oop.vmsbackend.utils.ResponseUtil;
 import io.jsonwebtoken.Claims;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.servlet.HandlerInterceptor;
+
+import java.io.IOException;
 
 public class BasicAuthInterceptor implements HandlerInterceptor {
 
   @Override
-  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-    if (!request.getRequestURI().contains("account")) {
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws IOException {
+    if (!request.getRequestURI().contains("account") && !request.getRequestURI().contains("error")) {
       try {
-        String token = request.getHeader("Authorization").split(" ")[1];
-        try {
-          Claims jwtValues = JwtUtil.verifyToken(token);
-          request.setAttribute("userId", jwtValues.get("id"));
-
-          return true;
-
-        } catch (Exception e){
-          System.out.println(e.getMessage());
-          return false;
+        String header = request.getHeader("Authorization").split(" ")[1];
+        System.out.println(header);
+        if (header == "") {
+          response.setStatus(HttpStatus.UNAUTHORIZED.value());
+          response.getWriter().write("User is forbidden to access");
+          return false; // stop the request from reaching the controller
         }
+        String token = header.split(" ")[1];
+        Claims jwtValues = JwtUtil.verifyToken(token);
+        request.setAttribute("userId", jwtValues.get("id"));
+        return true;
+
 
       } catch(Exception e){
-        System.out.println(e.getMessage());
-        if (e instanceof UserForbiddenException){
-          throw new UserForbiddenException(e.getMessage());
-        } else {
-          return false;
-        }
+        response.setStatus(HttpStatus.UNAUTHORIZED.value());
+        response.getWriter().write("User is forbidden to access");
+        return false;
+
       }
     }
 
     return true;
+
   }
 
   public boolean postHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-
+    System.out.println("POST");
     try {
       return true;
     } catch(Exception e){
