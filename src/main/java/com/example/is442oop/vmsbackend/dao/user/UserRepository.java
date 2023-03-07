e3ce182a2585ef24b066fc53720c4c6a7c151b13
Liye Ng <liye1998@hotmail.com>
2023-03-07 16:06:56 +0800

    added missing function

--- src/main/java/com/example/is442oop/vmsbackend/dao/user/UserRepository.java
+++ src/main/java/com/example/is442oop/vmsbackend/dao/user/UserRepository.java
@@ -2,9 +2,18 @@ package com.example.is442oop.vmsbackend.dao.user;
 
 import com.example.is442oop.vmsbackend.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
   Optional<User> findUserByEmail(String email);
+
+  @Modifying
+  @Transactional
+  @Query("UPDATE User u SET u.password = :password WHERE u.id = :id")
+  User updatePassword(@Param("id") Long id, @Param("password") String password);
 }
