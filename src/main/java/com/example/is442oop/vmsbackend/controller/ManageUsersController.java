f9a71e5ee23ec54ef4b45af490399b1856778088
Liye Ng <liye1998@hotmail.com>
2023-03-07 15:48:31 +0800

    updated getUserWorkflows

--- src/main/java/com/example/is442oop/vmsbackend/controller/ManageUsersController.java
+++ src/main/java/com/example/is442oop/vmsbackend/controller/ManageUsersController.java
@@ -1,21 +1,11 @@
 package com.example.is442oop.vmsbackend.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import com.example.is442oop.vmsbackend.entities.User;
-import com.example.is442oop.vmsbackend.entities.UserWorkflow;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestAttribute;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import com.example.is442oop.vmsbackend.dao.UserRepository;
 import com.example.is442oop.vmsbackend.service.manageusers.ManageUsersService;
 import com.example.is442oop.vmsbackend.utils.ResponseUtil;
 
@@ -34,13 +24,13 @@ public class ManageUsersController {
   // @RequestAttribute("userId") String id
   @GetMapping
   public ResponseEntity getAllUsers(){
-    return ResponseUtil.responseOk(manageUsersService.getAllUsers());
+    return ResponseUtil.responseGetUsersOk(manageUsersService.getAllUsers());
   }
   
-  // @GetMapping("/{userId}")
-  // public List<UserWorkflow> getUserWorkflows(@RequestAttribute("userId") Long userId) {
-  //   return manageUsersService.getUserWorkflows(userId); 
-  // }
+  @GetMapping("/{userId}")
+  public ResponseEntity getUserWorkflows(@PathVariable Long userId) {
+    return ResponseUtil.responseGetUserWorkflowsOk(manageUsersService.getUserWorkflows(userId)); 
+  }
 
   // @PostMapping("/{userId}")
   // public ResponseEntity assignWorkflows(@RequestBody ArrayList<Workflow> workflows) {
--- src/main/java/com/example/is442oop/vmsbackend/service/manageusers/ManageUsersService.java
+++ src/main/java/com/example/is442oop/vmsbackend/service/manageusers/ManageUsersService.java
@@ -1,40 +1,30 @@
 package com.example.is442oop.vmsbackend.service.manageusers;
 
-import com.example.is442oop.vmsbackend.authentication.BasicAuthInterceptor;
 import com.example.is442oop.vmsbackend.dao.UserDao;
-import com.example.is442oop.vmsbackend.dao.userworkflow.UserWorkflowDao;
 import com.example.is442oop.vmsbackend.entities.User;
 import com.example.is442oop.vmsbackend.entities.UserWorkflow;
-import com.example.is442oop.vmsbackend.exception.InternalServerException;
-import com.example.is442oop.vmsbackend.exception.NotFoundException;
-import com.example.is442oop.vmsbackend.exception.UserForbiddenException;
-import com.example.is442oop.vmsbackend.utils.ResponseUtil;
-
-import ch.qos.logback.core.recovery.ResilientSyslogOutputStream;
-
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ManageUsersService implements ManageUsersInterface {
 
   private final UserDao userDao;
-  private final UserWorkflowDao userworkflowDao;
 
   @Autowired
-  public ManageUsersService(UserDao userDao, UserWorkflowDao userworkflowDao){
+  public ManageUsersService(UserDao userDao){
     this.userDao = userDao;
-    this.userworkflowDao = userworkflowDao;
   }
   public List<User> getAllUsers() {
     return userDao.getAllUsers();
   }
 
   public List<UserWorkflow> getUserWorkflows(Long userid) {
-    return userworkflowDao.getUserWorkflows(userid);
+
+    User user = userDao.findUserById(userid);
+    return user.getUserWorkflows();
+
   }
 
 
