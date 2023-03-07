f4298dc1e3937ee92e80bc6c22023ba55a6da0b0
lanceljr <lanceljr98@gmail.com>
2023-03-07 14:28:11 +0800

    Added global cors config file

--- src/main/java/com/example/is442oop/vmsbackend/controller/QuestionnaireController.java
+++ src/main/java/com/example/is442oop/vmsbackend/controller/QuestionnaireController.java
@@ -18,7 +18,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/v1/questionnaires")
 public class QuestionnaireController {
