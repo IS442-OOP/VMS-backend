d45cfa04d4e2eac89621aa7d5a9094c6642ad17a
Liye Ng <liye1998@hotmail.com>
2023-03-08 19:18:00 +0800

    added UserWorkflowDao + UserWorkflowRepository

---
+++ src/main/java/com/example/is442oop/vmsbackend/dao/userworkflow/UserWorkflowRepository.java
@@ -0,0 +1,8 @@
+package com.example.is442oop.vmsbackend.dao.userworkflow;
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.example.is442oop.vmsbackend.entities.UserWorkflow;
+
+public interface UserWorkflowRepository extends JpaRepository<UserWorkflow, Long> {
+}
+
+
